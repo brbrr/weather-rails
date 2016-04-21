1f5b93c69d53aa1f8dcbf10144ce8e498494e068
Iaroslav Kukharuk <i.kukharuk@gmail.com>
2016-04-21 22:17:52 +0300

    assets stuff

--- config/application.rb
+++ config/application.rb
@@ -38,15 +38,15 @@ module WeatherRails
     # via https://gist.github.com/afeld/5704079
 
     # We don't want the default of everything that isn't js or css, because it pulls too many things in
-    # config.assets.precompile.shift
-    #
+    config.assets.precompile.shift
+    
     # # Explicitly register the extensions we are interested in compiling
-    # config.assets.precompile.push(proc do |path|
-    #   File.extname(path).in? [
-    #     '.html', '.erb', # '.haml',                 # Templates
-    #     # '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
-    #     # '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
-    #   ]
-    # end)
+    config.assets.precompile.push(proc do |path|
+      File.extname(path).in? [
+        '.html', '.erb', # '.haml',                 # Templates
+        # '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
+        # '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+      ]
+    end)
   end
 end
