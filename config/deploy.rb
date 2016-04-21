0f3372dd702aab010a5b18889e111446b636e763
Iaroslav Kukharuk <i.kukharuk@gmail.com>
2016-04-21 19:06:50 +0300

    fix css assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,10 +38,10 @@ namespace :deploy do
   task :bower_install do
       on roles(:app), in: :sequence, wait: 5 do
         within release_path do
-          execute :bower, "install"
+          execute :bower, 'install'
         end
       end
-    end
+  end
     after :published, :bower_install
 
   after :restart, :clear_cache do
