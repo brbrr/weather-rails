67796ce71669a5cb579fff40a96957affd935dd2
Iaroslav Kukharuk <i.kukharuk@gmail.com>
2016-04-22 00:57:04 +0300

    Switching to nginx

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,6 @@ module WeatherRails
 
     # We don't want the default of everything that isn't js or css, because it pulls too many things in
     config.assets.precompile.shift
-    
     # # Explicitly register the extensions we are interested in compiling
     config.assets.precompile.push(proc do |path|
       File.extname(path).in? [
--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 # set :puma_bind,       'tcp://0.0.0.0:3000'
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
